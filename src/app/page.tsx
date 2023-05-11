fe1a3790dafa8bb8c1465639d3f895b5bfd73ad9
Lila Rest <mail@lila.rest>
2023-05-11 14:41:59 +0200

    feat(layout): setup fonts and minimal metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import { type NextPage } from "next";
 
 const Page: NextPage = () => {
-  return <main className=" bg-amber-200">Home page</main>;
+  return (
+    <main className=" bg-amber-200">
+      Home page
+      <br />
+      Blabla <p>Yaaa</p>
+    </main>
+  );
 };
 export default Page;
