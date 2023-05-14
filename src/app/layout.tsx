1f21b8ec4c11cd569b55ddc9bdb5040a39243b06
Lila Rest <mail@lila.rest>
2023-05-14 12:10:55 +0200

    feat: import assets and add a minimal header for outside app layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import { Poppins, Inter } from "next/font/google";
 import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import logo from "~/assets/logo/light.png";
+import { Button } from "@/components/ui/button";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -29,13 +34,39 @@ interface Props {
 
 const RootLayout: NextPage<Props> = ({ children }) => {
   return (
-    <html lang="en">
-      <body className={`${inter.variable} ${poppins.variable}`}>
-        <header></header>
-        <main>{children}</main>
-        <footer></footer>
-      </body>
-    </html>
+    <>
+      <html lang="en">
+        <Head>
+          <title>Ledgity - DeFi Protocol</title>
+          <meta
+            name="description"
+            content="DeFi Protocol where you can lend USDC and start earning yield. Ledgity's stable yield comes from the best risk adjusted return opportunities from DeFi, CeFi and Real World Assets."
+          />
+          <link rel="icon" href="/assets/favicon/32x32.png" />
+        </Head>
+        <body className={`${inter.variable} ${poppins.variable}`}>
+          <header className="fixed z-10 w-screen">
+            <nav className="flex justify-between px-8 py-6 backdrop-blur-md">
+              <Link href="/" className="flex">
+                <Image
+                  alt="Ledgity Logo"
+                  src={logo}
+                  className="h-9 w-auto cursor-pointer"
+                />
+                <p className="-mt-0.5 ml-2 self-start rounded-md bg-sky-600/80 px-1.5 py-1 font-body text-[0.7rem] font-semibold leading-tight tracking-wide text-stone-100">
+                  DeFi
+                </p>
+              </Link>
+              <Link href="/app">
+                <Button size="large">Enter app</Button>
+              </Link>
+            </nav>
+          </header>
+          <main className="pt-8">{children}</main>
+          <footer></footer>
+        </body>
+      </html>
+    </>
   );
 };
 
--- src/app/ui/page.tsx
+++ src/app/ui/page.tsx
@@ -4,29 +4,25 @@ import { NextPage } from "next";
 const Page: NextPage = () => {
   return (
     <>
-      <header className="relative p-8">
+      <div className="flex flex-col items-center p-14 gap-14">
         <h1 className="text-3xl font-bold text-center">Ledgity DApp UI kit</h1>
-
-        {/* Theme switcher */}
-        <div className="absolute right-8 top-8">X</div>
-      </header>
-
-      <main className="flex items-center p-14 gap-14">
-        <section>
-          <h2 className="text-2xl text-center font-semibold">Buttons</h2>
-          <div className="p-2">
-            {buttonVariants.map((variant, index1) => (
-              <div key={index1} className="flex gap-4 p-2">
-                {buttonSizes.map((size, index2) => (
-                  <Button variant={variant} size={size as any} key={index2}>
-                    {variant} {size}
-                  </Button>
-                ))}
-              </div>
-            ))}
-          </div>
-        </section>
-      </main>
+        <div className="flex w-screen p-8">
+          <section>
+            <h2 className="text-2xl text-center font-semibold">Buttons</h2>
+            <div className="p-2">
+              {buttonVariants.map((variant, index1) => (
+                <div key={index1} className="flex gap-4 p-2">
+                  {buttonSizes.map((size, index2) => (
+                    <Button variant={variant} size={size as any} key={index2}>
+                      {variant} {size}
+                    </Button>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </section>
+        </div>
+      </div>
     </>
   );
 };
