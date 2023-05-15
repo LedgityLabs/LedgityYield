81d5467985a9a1b11d5b6b35b204c4fd84635664
Lila Rest <mail@lila.rest>
2023-05-15 12:22:51 +0200

    feat(home): temporarily remove header backdrop blur effect for a more immersive experience

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -14,19 +14,19 @@ const Page: NextPage = () => (
       </h2>
       <section className="relative my-20 flex content-around justify-around">
         {/* <div className=" absolute top-8 h-1 w-screen bg-sky-200"></div> */}
-        <article className=" relative mx-8 flex h-40 w-44 flex-col items-center justify-center rounded-3xl bg-primary/5 px-6 py-6 backdrop-blur-lg text-fg/80 shadow-sm shadow-slate-200 border-input/20 border-2">
+        <article className=" relative mx-8 flex h-40 w-44 flex-col items-center justify-center rounded-3xl bg-primary/5 px-6 py-6 backdrop-blur-md text-fg/80 shadow-sm shadow-slate-200 border-input/20 border-2">
           <p className="mb-3 text-5xl font-bold">$8k</p>
           <h3 className="absolute bottom-3 text-lg  font-semibold text-primary/30">
             TVL
           </h3>
         </article>
-        <article className=" relative mx-8 flex h-40 w-44 flex-col items-center justify-center rounded-3xl bg-primary/5 px-6 py-6 backdrop-blur-lg text-fg/80 shadow-sm shadow-slate-200 border-input/20 border-2">
+        <article className=" relative mx-8 flex h-40 w-44 flex-col items-center justify-center rounded-3xl bg-primary/5 px-6 py-6 backdrop-blur-md text-fg/80 shadow-sm shadow-slate-200 border-input/20 border-2">
           <p className="mb-3 text-5xl font-bold">7%</p>
           <h3 className="absolute bottom-3 text-lg  font-semibold text-primary/30">
             APY
           </h3>
         </article>
-        <article className=" relative mx-8 flex h-40 w-44 flex-col items-center justify-center rounded-3xl bg-primary/5 px-6 py-6 backdrop-blur-lg text-fg/80 shadow-sm shadow-slate-200 border-input/20 border-2">
+        <article className=" relative mx-8 flex h-40 w-44 flex-col items-center justify-center rounded-3xl bg-primary/5 px-6 py-6 backdrop-blur-md text-fg/80 shadow-sm shadow-slate-200 border-input/20 border-2">
           <p className="mb-3 text-5xl font-bold">±0.1%</p>
           <h3 className="absolute bottom-3 text-lg  font-semibold text-primary/30">
             1Y stability
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,23 +4,26 @@ import Image from "next/image";
 import logo from "~/assets/logo/light.png";
 import { Button } from "@/components/ui";
 
-const Header: FC = () => (
-  <header className="pb-[92px] relative">
-    <nav className="fixed z-10 flex justify-between px-8 py-6 backdrop-blur-md w-screen">
-      <Link href="/" className="flex pt-1">
-        <Image
-          alt="Ledgity Logo"
-          src={logo}
-          className="h-9 w-auto cursor-pointer"
-        />
-        <p className="-mt-0.5 ml-2 self-start rounded-md bg-indigo-400 px-1.5 py-1 font-body text-[0.7rem] font-semibold leading-tight tracking-wide text-stone-100">
-          DeFi
-        </p>
-      </Link>
-      <Link href="/app">
-        <Button size="large">Enter app</Button>
-      </Link>
-    </nav>
-  </header>
-);
+const Header: FC = () => {
+  // TODO: Add "backdrop-blur-md" tailwind class to `<nav>` element when its 'y' position !== 0
+  return (
+    <header className="pb-[92px] relative">
+      <nav className="fixed z-10 flex justify-between px-8 py-6 w-screen">
+        <Link href="/" className="flex pt-1">
+          <Image
+            alt="Ledgity Logo"
+            src={logo}
+            className="h-9 w-auto cursor-pointer"
+          />
+          <p className="-mt-0.5 ml-2 self-start rounded-sm bg-indigo-400 px-1.5 py-1 font-body text-[0.7rem] font-semibold leading-tight tracking-wide text-stone-100">
+            DeFi
+          </p>
+        </Link>
+        <Link href="/app">
+          <Button size="large">Enter app</Button>
+        </Link>
+      </nav>
+    </header>
+  );
+};
 export default Header;
