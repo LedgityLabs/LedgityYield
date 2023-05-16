174718bb95d92339bad4299120e136bd8d3a1b54
Lila Rest <mail@lila.rest>
2023-05-16 16:37:30 +0200

    feat(ui): add Card component to the ui kit page

--- src/app/ui/page.tsx
+++ src/app/ui/page.tsx
@@ -1,12 +1,20 @@
-import { Button, buttonSizes, buttonVariants } from "@/components/ui";
+import {
+  Button,
+  buttonSizes,
+  buttonVariants,
+  Card,
+  cardVariants,
+} from "@/components/ui";
 import { NextPage } from "next";
 
 const Page: NextPage = () => {
   return (
     <>
-      <div className="flex flex-col items-center p-14 gap-14">
-        <h1 className="text-3xl font-bold text-center">Ledgity DApp UI kit</h1>
-        <div className="flex w-screen p-8">
+      <div className="flex flex-col items-center p-14">
+        <h1 className="text-3xl font-bold text-center mb-12">
+          Ledgity DApp UI kit
+        </h1>
+        <div className="flex w-screen p-12 gap-12">
           <section>
             <h2 className="text-2xl text-center font-semibold">Buttons</h2>
             <div className="p-2">
@@ -18,6 +26,23 @@ const Page: NextPage = () => {
                     </Button>
                   ))}
                 </div>
+              ))}
+            </div>
+          </section>
+          <section>
+            <h2 className="text-2xl text-center font-semibold">Cards</h2>
+            <div className="p-2">
+              {cardVariants.map((variant, index) => (
+                <Card variant={variant} key={index} className="max-w-lg">
+                  <h3 className="font-bold text-xl text-center">{variant}</h3>
+                  <br />
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  </p>
+                </Card>
               ))}
             </div>
           </section>
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,2 +1,3 @@
 export * from "./Button";
 export * from "./Scroller";
+export * from "./Card";
