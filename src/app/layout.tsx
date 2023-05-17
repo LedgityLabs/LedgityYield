43af5f20c7881ff3e6494aca88fa738b58b9c3fe
Lila Rest <mail@lila.rest>
2023-05-17 18:05:17 +0200

    feat: add favicon + replace usage of deprecated next/head by metadata API

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "@/styles/globals.css";
 import { Poppins, Inter } from "next/font/google";
 import { type NextPage } from "next";
-import Head from "next/head";
 import Header from "@/components/Header";
 import { CardsHelper } from "@/components/ui";
 
@@ -21,7 +20,7 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "Ledgity - DeFi Protocol",
+  title: "Ledgity DeFi Protocol",
   description:
     "Invest stablecoins into real world assets and earn up to 7% APY. Access low-risk & stable yield offered by real world asset directly from your wallet.",
 };
@@ -34,14 +33,6 @@ const RootLayout: NextPage<Props> = ({ children }) => {
   return (
     <>
       <html lang="en">
-        <Head>
-          <title>Ledgity - DeFi Protocol</title>
-          <meta
-            name="description"
-            content="Invest stablecoins into real world assets and earn up to 7% APY. Access low-risk & stable yield offered by real world assets directly from your wallet."
-          />
-          <link rel="icon" href="/assets/favicon/32x32.png" />
-        </Head>
         <CardsHelper />
         <body className={`${inter.variable} ${poppins.variable}`}>
           <Header />
