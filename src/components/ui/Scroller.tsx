aca3dbb5d9823083d92b14041e8c1338314c18d1
Lila Rest <mail@lila.rest>
2023-05-18 22:15:19 +0200

    feat(home): progessively hide scroll indicator onscroll

--- src/components/ui/Scroller.tsx
+++ src/components/ui/Scroller.tsx
@@ -1,18 +1,32 @@
-import { FC } from "react";
+"use client";
+import { FC, useEffect, useState } from "react";
 import { twMerge } from "tailwind-merge";
 
 export interface ScrollerProps {
   className?: string;
 }
 
-export const Scroller: FC<ScrollerProps> = ({ className }) => (
-  <div
-    className={twMerge(
-      "border-4 rounded-lg border-fg/70 border-solid h-10 w-6 p-1",
-      className
-    )}
-  >
-    <div className="animate-roll bg-fg/70 w-2 h-2 rounded-full"></div>
-  </div>
-);
+export const Scroller: FC<ScrollerProps> = ({ className }) => {
+  const [opacity, setOpacity] = useState(0);
+  const handleScroll = () =>
+    setOpacity(1 - (window.scrollY / screen.height) * 2);
+
+  useEffect(() => {
+    handleScroll();
+    document.addEventListener("scroll", handleScroll);
+    return () => document.removeEventListener("scroll", handleScroll);
+  });
+
+  return (
+    <div
+      style={{ opacity: opacity }}
+      className={twMerge(
+        "border-4 rounded-lg border-fg/70 border-solid h-10 w-6 p-1",
+        className
+      )}
+    >
+      <div className="animate-roll bg-fg/70 w-2 h-2 rounded-full"></div>
+    </div>
+  );
+};
 export default Scroller;
