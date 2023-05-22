b3e218e6faeef174281ab69d0568fb5ef8ebb723
Lila Rest <mail@lila.rest>
2023-05-22 21:27:14 +0200

    feat(home): make features cards and "how it works?" section responsive

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -18,9 +18,9 @@ const Page: NextPage = () => (
         "after:bg-gradient-to-b after:from-transparent after:to-bg after:absolute after:top-[100vh] after:w-screen after:h-[40vh] after:-z-0"
       )}
     >
-      <div className="flex -mt-[92px] relative flex-col min-h-screen justify-center items-center lg:gap-20 md:gap-12 gap-14">
-        <h2 className="text-center leading-none font-heading lg:text-8xl md:text-[4.5rem] sm:text-[4rem] text-[2.5rem] font-bold text-slate-700 whitespace-nowrap">
-          Stable yields for
+      <div className="flex -mt-[92px] relative flex-col min-h-screen justify-center items-center lg:gap-18 md:gap-12 gap-14">
+        <h2 className="text-center leading-none font-heading lg:text-8xl md:text-[4.5rem] sm:text-[4rem] text-[3rem] font-bold text-slate-700">
+          Stable <span className="whitespace-nowrap">yields for</span>
           <br />
           <span className="text-indigo-300 text-transparent bg-clip-text bg-gradient-to-t from-indigo-300 to-indigo-500">
             stablecoins
@@ -29,15 +29,15 @@ const Page: NextPage = () => (
         </h2>
         <section className="relative flex content-around justify-around lg:gap-16 md:gap-10 sm:gap-8 gap-4">
           {/* <div className=" absolute top-8 h-1 w-screen bg-sky-200"></div> */}
-          <Card className="relative flex lg:h-40 lg:w-44 sm:h-28 sm:w-32 h-24 w-24 flex-col items-center justify-center p-6">
+          <Card className="relative flex lg:h-40 lg:w-44 sm:h-32 sm:w-32 h-24 w-24 flex-col items-center justify-center p-6">
             <p className="mb-3 lg:text-5xl md:text-4xl sm:text-4xl text-2xl font-bold text-fg/80">$8k</p>
             <h3 className="absolute bottom-3 sm:text-lg text-sm font-semibold text-primary/30">TVL</h3>
           </Card>
-          <Card className="relative flex lg:h-40 lg:w-44 sm:h-28 sm:w-32 h-24 w-24 flex-col items-center justify-center p-6">
+          <Card className="relative flex lg:h-40 lg:w-44 sm:h-32 sm:w-32 h-24 w-24 flex-col items-center justify-center p-6">
             <p className="mb-3 lg:text-5xl md:text-4xl sm:text-4xl text-2xl font-bold text-fg/80">7%</p>
             <h3 className="absolute bottom-3 sm:text-lg text-sm font-semibold text-primary/30">APY</h3>
           </Card>
-          <Card className="relative flex lg:h-40 lg:w-44 sm:h-28 sm:w-32 h-24 w-24 flex-col items-center justify-center p-6">
+          <Card className="relative flex lg:h-40 lg:w-44 sm:h-32 sm:w-32 h-24 w-24 flex-col items-center justify-center p-6">
             <p className="mb-3 lg:text-5xl md:text-4xl sm:text-4xl text-2xl font-bold text-fg/80">±0.1%</p>
             <h3 className="absolute bottom-3 sm:text-lg text-sm font-semibold text-primary/30">1Y stability</h3>
           </Card>
@@ -55,9 +55,9 @@ const Page: NextPage = () => (
     </section>
     <div className="bg-[url('/assets/other-glow.png')] bg-cover bg-top relative">
       <section className="flex flex-col justify-center items-center -mt-56">
-        <div className="grid grid-cols-3 gap-12 px-8 ">
+        <div className="grid xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-12 px-8 ">
           <div>
-            <Card className="w-96 flex justify-between flex-col overflow-hidden opacity-90">
+            <Card className="lg:w-92 sm:w-96 w-full flex justify-between flex-col overflow-hidden opacity-90">
               <Image className="invert opacity-90" src={analytics} alt="todo" />
               <h3 className="mb-2 text-center mt-6 text-fg/90 font-bold text-[1.3rem] font-heading">
                 Long term stability
@@ -69,7 +69,7 @@ const Page: NextPage = () => (
             </Card>
           </div>
           <div>
-            <Card className="w-96 flex justify-between flex-col overflow-hidden opacity-90">
+            <Card className="lg:w-92 sm:w-96 w-full flex justify-between flex-col overflow-hidden opacity-90">
               <Image className="invert opacity-90" src={analytics} alt="todo" />
               <h3 className="mb-2 text-center mt-6 text-fg/90 font-bold text-[1.3rem] font-heading">High efficiency</h3>
               <p className="p-6 pt-0 pb-7 text-lg text-center">
@@ -79,7 +79,7 @@ const Page: NextPage = () => (
             </Card>
           </div>
           <div>
-            <Card className="w-96 flex justify-between flex-col overflow-hidden opacity-90">
+            <Card className="lg:w-92 sm:w-96 w-full flex justify-between flex-col overflow-hidden opacity-90">
               <Image className="invert opacity-90" src={analytics} alt="todo" />
               <h3 className="mb-2 text-center mt-6 text-fg/90 font-bold text-[1.3rem] font-heading">
                 Diversified portofolio
@@ -90,7 +90,7 @@ const Page: NextPage = () => (
             </Card>
           </div>
           <div>
-            <Card className="w-96 flex justify-between flex-col overflow-hidden opacity-90">
+            <Card className="lg:w-92 sm:w-96 w-full flex justify-between flex-col overflow-hidden opacity-90">
               <Image className="invert opacity-90" src={analytics} alt="todo" />
               <h3 className="mb-2 text-center mt-6 text-fg/90 font-bold text-[1.3rem] font-heading">
                 Detailed analytics
@@ -101,7 +101,7 @@ const Page: NextPage = () => (
             </Card>
           </div>
           <div>
-            <Card className="w-96 flex justify-between flex-col overflow-hidden opacity-90">
+            <Card className="lg:w-92 sm:w-96 w-full flex justify-between flex-col overflow-hidden opacity-90">
               <Image className="invert opacity-90" src={analytics} alt="todo" />
               <h3 className="mb-2 text-center mt-6 text-fg/90 font-bold text-[1.3rem] font-heading">Heavily audited</h3>
               <p className="p-6 pt-0 pb-7 text-lg text-center">
@@ -111,7 +111,7 @@ const Page: NextPage = () => (
             </Card>
           </div>
           <div>
-            <Card className="w-96 flex justify-between flex-col overflow-hidden opacity-90">
+            <Card className="lg:w-92 sm:w-96 w-full flex justify-between flex-col overflow-hidden opacity-90">
               <Image className="invert opacity-90" src={analytics} alt="todo" />
               <h3 className="mb-2 text-center mt-6 text-fg/90 font-bold text-[1.3rem] font-heading">Multi-chains</h3>
               <p className="p-6 pt-0 pb-7 text-lg text-center">
@@ -129,42 +129,46 @@ const Page: NextPage = () => (
       </section>
       <section className="flex flex-col justify-center items-center py-48">
         <h3 className="text-center font-semibold text-4xl pb-4 font-heading">How it works ?</h3>
-        <ol className="flex flex-col justify-center items-center flex-wrap">
-          <li className="p-4">
-            <Card className="p-8 flex gap-8 items-center w-[500px]">
+        <ol className="flex flex-col justify-center items-center flex-wrap gap-8 p-8">
+          <li>
+            <Card className="p-8 flex sm:flex-row flex-col justift-center gap-8 items-center sm:w-[500px] w-full">
               <Card
                 radius="full"
                 className="text-xl h-12 w-12 bg-primary/80 rounded-full flex justify-center items-center"
               >
                 <span className="text-primary-fg font-bold">1</span>
               </Card>
-              <p className="text-xl">You lend stablecoins and get wrapped coins as proof of deposit</p>
+              <p className="text-xl sm:text-start text-center">
+                You lend stablecoins and get wrapped coins as proof of deposit
+              </p>
             </Card>
           </li>
-          <li className="p-4">
-            <Card className="p-8 flex gap-8 items-center w-[500px]">
+          <li>
+            <Card className="p-8 flex sm:flex-row flex-col justift-center gap-8 items-center sm:w-[500px] w-full">
               <Card
                 radius="full"
                 className="text-xl h-12 w-12 bg-primary/80 rounded-full flex justify-center items-center"
               >
                 <span className="text-primary-fg font-bold">2</span>
               </Card>
-              <p className="text-xl">Your funds get allocated accross diversified DeFi and CeFi strategies</p>
+              <p className="text-xl sm:text-start text-center">
+                Your funds get allocated accross diversified DeFi and CeFi strategies
+              </p>
             </Card>
           </li>
-          <li className="basis-full p-4 flex justify-center">
-            <Card className="p-8 flex gap-8 items-center w-[500px]">
+          <li>
+            <Card className="p-8 flex sm:flex-row flex-col justift-center gap-8 items-center sm:w-[500px] w-full">
               <Card
                 radius="full"
                 className="text-xl h-12 w-12 bg-primary/80 rounded-full flex justify-center items-center"
               >
                 <span className="text-primary-fg font-bold">3</span>
               </Card>
-              <p className="text-xl">You start earning stable yields while you sleep !</p>
+              <p className="text-xl sm:text-start text-center">You start earning stable yields while you sleep !</p>
             </Card>
           </li>
         </ol>
-        <div className="flex gap-8 justify-center items-center mt-8">
+        <div className="flex sm:flex-row flex-col gap-8 justify-center items-center mt-8">
           <p className="text-xl font-semibold underline decoration-2 decoration-slate-400">Ready to get started ?</p>
           <Link href="/app">
             <Button size="large">Take me to the app</Button>
