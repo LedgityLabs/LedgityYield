d15deba29077dc2de51843d566de3e7d97c6eaca
Lila Rest <mail@lila.rest>
2023-06-30 12:39:11 +0200

    feat: give some style to error page

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Button } from "@/components/ui";
 import { type NextPage } from "next";
 import Link from "next/link";
 
@@ -10,11 +11,32 @@ interface Props {
 
 const Page: NextPage<Props> = ({ error, reset }) => {
   return (
-    <main className="flex flex-col">
-      <h2>{error.message || "Something went wrong"}</h2>
-      <button onClick={reset}>Retry</button>
-      <Link href="/">Go back home</Link>
-      <Link href="/app">Go back app</Link>
+    <main className="flex justify-center items-center min-h-screen">
+      <div className="w-[90vw] max-w-[380px] flex flex-col justify-center items-center gap-16 transform scale-90 sm:scale-100 ">
+        <h1 className="text-8xl font-extrabold font-heading text-center text-primary">
+          Render
+          <br />
+          Error
+        </h1>
+        <div className="flex flex-col justify-center items-center gap-3">
+          <p className="text-lg font-medium text-center whitespace-nowrap">
+            Oops! An error occured while rendering
+            <br />
+            this page, with the following reason:
+          </p>
+          <code className="p-8 bg-fg text-bg font-['monospace'] rounded-3xl ">
+            {error.message || "Unknown error"}
+          </code>
+        </div>
+        <div className="flex gap-6 justify-center items-center">
+          <Link href="/">
+            <Button>Go back</Button>
+          </Link>
+          <Button variant="outline" onClick={reset}>
+            Retry
+          </Button>
+        </div>
+      </div>
     </main>
   );
 };
