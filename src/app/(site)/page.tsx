9e3c8c2894138b0a4f291f65e5fc2e182ea5066c
Lila Rest <mail@lila.rest>
2023-07-01 16:52:50 +0200

    fix(app): remove USD rate console log

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -3,7 +3,6 @@ import HomeHero from "@/components/site/home/HomeHero";
 import HomePartners from "@/components/site/home/HomePartners";
 import HomeHowItWorks from "@/components/site/home/HomeHowItWorks";
 import HomeFeatures from "@/components/site/home/HomeFeatures";
-import HomeCTA from "@/components/site/home/HomeCTA";
 
 export const metadata = {
   title: "Stable Yield For Stablecoins",
@@ -16,7 +15,6 @@ const Page: NextPage = () => {
       <div className="relative bg-[url('/assets/other-glow.webp')] bg-cover md:bg-top bg-[left_30%_top_30%] md:top-[90vh] top-[100vh] md:pb-[90vh] pb-[100vh]">
         <HomeFeatures />
         <HomeHowItWorks />
-        {/* <HomeCTA /> */}
         <HomePartners />
       </div>
     </>
