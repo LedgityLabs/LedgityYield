ada81b9fb7c1ce0a0bd5bceaef1008456ecca65f
Lila Rest <mail@lila.rest>
2023-07-10 18:29:23 +0200

    docs(contracts): improve documentation of BaseUpgradeable

--- hardhat/contracts/abstracts/base/BaseUpgradeable.sol
+++ hardhat/contracts/abstracts/base/BaseUpgradeable.sol
@@ -8,6 +8,14 @@ import {RecoverableUpgradeable} from "../RecoverableUpgradeable.sol";
 import {PausableUpgradeable} from "../pausable/PausableUpgradeable.sol";
 import {OwnableUpgradeable} from "../OwnableUpgradeable.sol";
 
+/**
+ * @title BaseUpgradeable
+ * @author Lila Rest (lila@ledgity.com)
+ * @notice This abstract contract serves as a base for many contract in this code base. It
+ * reduces code repetition, makes code more readable and so maintainable.
+ * @dev For further details, see "Base contracts" section of whitepaper.
+ * @custom:security-contact security@ledgity.com
+ */
 abstract contract BaseUpgradeable is
     Initializable,
     UUPSUpgradeable,
@@ -16,13 +24,20 @@ abstract contract BaseUpgradeable is
     RestrictableUpgradeable,
     RecoverableUpgradeable
 {
-    /// @custom:oz-upgrades-unsafe-allow constructor
+    /**
+     * @dev Prevent implementation contract from being initialized as recommended by
+     * OpenZeppelin.
+     * See: https://docs.openzeppelin.com/contracts/4.x/api/proxy#Initializable-_disableInitializers--
+     * @custom:oz-upgrades-unsafe-allow constructor
+     */
     constructor() {
         _disableInitializers();
     }
 
     /**
-     * @dev Initializer function ...
+     * @dev Initializer function of the contract. It replace the constructor function
+     * in context of upgradeable contracts.
+     * See: https://docs.openzeppelin.com/contracts/4.x/upgradeable
      */
     function __Base_init(address _globalOwner) internal onlyInitializing {
         __UUPSUpgradeable_init();
@@ -30,6 +45,11 @@ abstract contract BaseUpgradeable is
         __Ownable_init(_globalOwner);
     }
 
+    /**
+     * Override of UUPSUpgradeable._authorizeUpgrade() function restricted to the global
+     * owner. Note that this function is called by the proxy contract when upgrading.
+     * @param newImplementation The address of the new implementation contract
+     */
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
     /**
