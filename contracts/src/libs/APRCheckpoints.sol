17d49c253cbd543bcb1cd165bc9b878a96b4d616
Lila Rest <mail@lila.rest>
2023-08-05 17:46:45 +0200

    fix: fix contract name in APRCheckpoints docs

--- contracts/src/libs/APRCheckpoints.sol
+++ contracts/src/libs/APRCheckpoints.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 /**
- * @title SUD
+ * @title APRCheckpoints
  * @author Lila Rest (lila@ledgity.com)
  * @notice This library provides utilities to create and interact with APR checkpoints.
  * APR checkpoints are basically a way to efficiently store an history of APRs on-chain.
