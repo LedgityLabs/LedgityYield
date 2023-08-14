36e77a141314e0a49877d97b170ef695a1ad60a9
Lila Rest <mail@lila.rest>
2023-08-14 08:49:20 +0200

    feat: popoulate Get USDC cards

--- src/components/app/get-usdc/AppBorrowUSDC.tsx
+++ src/components/app/get-usdc/AppBorrowUSDC.tsx
@@ -4,12 +4,23 @@ import { twMerge } from "tailwind-merge";
 
 export const AppBorrowUSDC: FC = () => {
   return (
-    <Card circleIntensity={0.07} className={twMerge("flex flex-col items-center px-4 pb-6 pt-10")}>
+    <Card circleIntensity={0.07} className={twMerge("flex flex-col items-center p-10")}>
       <h2 className="inline-flex items-center justify-center gap-3 pb-4 text-center font-heading text-2xl font-bold text-fg/90">
         Borrow USDC
         <i className="ri-hand-coin-fill text-3xl" />
       </h2>
       <p className="text-lg font-semibold text-primary">Borrow USDC against collateral.</p>
+      <div className="mt-10 flex h-full w-full flex-grow flex-col gap-8">
+        <div className="flex h-full w-full items-center justify-center rounded-3xl bg-gradient-to-bl from-fg/10 to-fg/25 text-lg font-semibold text-fg/70 opacity-70">
+          Partner to be revealed
+        </div>
+        <div className="flex h-full w-full items-center justify-center rounded-3xl bg-gradient-to-bl from-fg/10 to-fg/25 text-lg font-semibold text-fg/70 opacity-70">
+          Partner to be revealed
+        </div>
+        <div className="flex h-full w-full items-center justify-center rounded-3xl bg-gradient-to-bl from-fg/10 to-fg/25 text-lg font-semibold text-fg/70 opacity-70">
+          Partner to be revealed
+        </div>
+      </div>
     </Card>
   );
 };
