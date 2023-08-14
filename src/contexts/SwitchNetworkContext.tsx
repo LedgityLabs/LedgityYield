83fcef57bb3b6a9db6a27c9df890bbed6222a684
Lila Rest <mail@lila.rest>
2023-08-14 06:51:09 +0200

    feat(app): add prefered chain cookie and campaign URL parameter

--- src/contexts/SwitchNetworkContext.tsx
+++ src/contexts/SwitchNetworkContext.tsx
@@ -2,8 +2,12 @@
 import { FC, createContext } from "react";
 import { chainAwarePublicClient } from "@/components/app/DApp";
 import { useState } from "react";
-import { useSwitchNetwork } from "wagmi";
+import { usePublicClient, useSwitchNetwork } from "wagmi";
 import { getConfig } from "@wagmi/core";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { setCookie, getCookie } from "cookies-next";
+import { useDisconnect } from "wagmi";
 
 interface ISwitchNetworkContext {
   switchNetwork: (id: string | number) => void;
@@ -18,30 +22,67 @@ interface Props {
 
 export const SwitchNetworkProvider: FC<Props> = ({ children }) => {
   const config = getConfig();
+  const publicClient = usePublicClient();
   const { switchNetworkAsync: switchWalletNetwork } = useSwitchNetwork();
   const [isSwitching, setIsSwitching] = useState(false);
+  const searchParams = useSearchParams();
 
   const switchNetwork = async (id: string | number) => {
     setIsSwitching(true);
+
     // Ensure the id is a number
     if (typeof id === "string") id = Number.parseInt(id);
-    let error: Error | null = null;
-    // If a wallet is connected, switch the wallet network
-    if (switchWalletNetwork) {
-      await switchWalletNetwork(id).catch((e: Error) => {
-        // Ignore the error if it's a user rejected switch
-        if (!e.message.includes("rejected")) error = e;
-      });
+
+    // Ignore if trying to switch to the same network
+    if (publicClient && publicClient.chain.id !== id) {
+      let error: Error | null = null;
+      // If a wallet is connected, switch the wallet network
+      if (switchWalletNetwork) {
+        console.log("SWITCH WALLET");
+        await switchWalletNetwork(id).catch((e: Error) => {
+          // Ignore the error if it's a user rejected switch
+          if (!e.message.includes("rejected")) error = e;
+        });
+      }
+      // If no wallet is connected or wallet switch was successful
+      if (!error) {
+        // Switch public client
+        config.setPublicClient((config: { chainId?: number }) =>
+          chainAwarePublicClient({ chainId: config.chainId || (id as number) }),
+        );
+      }
     }
-    // If no wallet is connected or wallet switch was successful
-    if (!error) {
-      // Switch public client
-      config.setPublicClient((config: { chainId?: number }) =>
-        chainAwarePublicClient({ chainId: config.chainId || (id as number) }),
-      );
-    }
+
     setIsSwitching(false);
   };
+
+  const handleDefault = () => {
+    const chain = searchParams.get("c");
+    let hasBeenSet = false;
+    if (chain) {
+      if (chain === "l") {
+        setCookie("preferedChain", "59144");
+        switchNetwork(59144);
+        hasBeenSet = true;
+      } else if (chain === "a") {
+        setCookie("preferedChain", "42161");
+        switchNetwork(42161);
+        hasBeenSet = true;
+      }
+    }
+    if (!hasBeenSet && getCookie("preferedChain")) {
+      switchNetwork(getCookie("preferedChain")!.toString());
+    }
+  };
+
+  // If the user comes from a prefered chain, set it as the default chain
+  useEffect(() => {
+    handleDefault();
+  }, [switchWalletNetwork]);
+
+  useEffect(() => {
+    handleDefault();
+  }, []);
 
   return (
     <SwitchNetworkContext.Provider
