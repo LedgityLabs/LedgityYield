c0557d7ddca11b9422fa2fc9368c9af96a781ece
Lila Rest <mail@lila.rest>
2023-08-15 14:01:42 +0200

    docs: improve LTokenSignaler docs

--- contracts/src/LTokenSignaler.sol
+++ contracts/src/LTokenSignaler.sol
@@ -8,22 +8,27 @@ import {LToken} from "./LToken.sol";
 
 /**
  * @title LTokenSignaler
- * @author Lila Rest (lila@ledgity.com)
- * @dev This contract is used to signal a L-Token contract to the TheGraph subgraph
- * of the current chain. If the L-Token contract wasn't already indexed, it will
- * start being indexed after this signal. Else, the signal will be ignored.
+ * @author Lila Rest (https://lila.rest)
+ * @custom:security-contact security@ledgity.com
+ *
+ * @notice Used to inform subgraph from the existence of a new L-Token contract. Once
+ * signaled, a L-Token it will start being indexed.
+ *
+ * @dev Signal are ignored by the subgraph if the L-Token is already known by it.
+ *
+ * @custom:security-contact security@ledgity.com
  */
 contract LTokenSignaler is Initializable, UUPSUpgradeable, GlobalOwnableUpgradeable {
     /**
-     * @dev Emitted to inform subgraph of the existence of a new L-Token contract.
-     * @param lTokenAddress The address of the L-Token contract to signal
+     * @notice Emitted to inform subgraph of the existence of a new L-Token contract.
+     * @param lTokenAddress The address of the L-Token contract to signal.
      */
     event LTokenSignalEvent(address indexed lTokenAddress);
 
     /**
-     * @dev Prevents implementation contract from being initialized as recommended by
+     * @notice Prevents implementation contract from being initialized as recommended by
      * OpenZeppelin.
-     * See: https://docs.openzeppelin.com/contracts/4.x/api/proxy#Initializable-_disableInitializers--
+     * @dev See: https://docs.openzeppelin.com/contracts/4.x/api/proxy#Initializable-_disableInitializers--
      * @custom:oz-upgrades-unsafe-allow constructor
      */
     constructor() {
@@ -31,9 +36,9 @@ contract LTokenSignaler is Initializable, UUPSUpgradeable, GlobalOwnableUpgradea
     }
 
     /**
-     * @dev Replaces the constructor() function in context of an upgradeable contract.
-     * See: https://docs.openzeppelin.com/contracts/4.x/upgradeable
-     * @param globalOwner_ The address of the GlobalOwner contract
+     * @notice Initializer function of the contract. It replaces the constructor()
+     * function in the context of upgradeable contracts.
+     * @dev See: https://docs.openzeppelin.com/contracts/4.x/upgradeable
      */
     function initialize(address globalOwner_) public initializer {
         __GlobalOwnable_init(globalOwner_);
@@ -41,15 +46,15 @@ contract LTokenSignaler is Initializable, UUPSUpgradeable, GlobalOwnableUpgradea
     }
 
     /**
-     * @dev Override of UUPSUpgradeable._authorizeUpgrade() function restricted to the global
-     * owner. Note that this function is called by the proxy contract while upgrading.
-     * @param newImplementation The address of the new implementation contract
+     * @notice Override of UUPSUpgradeable._authorizeUpgrade() function restricted to
+     * global owner. It is called by the proxy contract during an upgrade.
+     * @param newImplementation The address of the new implementation contract.
      */
     function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 
     /**
-     * @dev Signals a LToken contract to the TheGraph subgraph of the current chain.
-     * @param lTokenAddress The address of the LToken contract to signal
+     * @notice Signals a LToken contract to the TheGraph subgraph of the current chain.
+     * @param lTokenAddress The address of the LToken contract to signal.
      */
     function signalLToken(address lTokenAddress) external onlyOwner {
         // Signal the LToken contract
