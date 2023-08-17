9242dcd1caae20660df24aac180000d01e68e994
Lila Rest <mail@lila.rest>
2023-08-17 19:53:39 +0200

    feat: updated partners list

--- src/components/site/home/HomePartners.tsx
+++ src/components/site/home/HomePartners.tsx
@@ -2,11 +2,16 @@ import Image from "next/image";
 import { FC } from "react";
 import { Cube, FadeIn } from "@/components/ui";
 import Link from "next/link";
-import foundersoneLogo from "~/assets/partners/foundersone.png";
-import delubacLogo from "~/assets/partners/delubac.png";
-import risepartnersLogo from "~/assets/partners/risepartners.png";
 import adanLogo from "~/assets/partners/adan.svg";
+import arbitrumLogo from "~/assets/partners/arbitrum.svg";
 import circleLogo from "~/assets/partners/circle.png";
+import delubacLogo from "~/assets/partners/delubac.png";
+import foundersDAOLogo from "~/assets/partners/founders-dao.png";
+import geniusXLogo from "~/assets/partners/genius-x.png";
+import hederaLogo from "~/assets/partners/hedera.png";
+import lineaLogo from "~/assets/partners/linea.png";
+import offchainLabsLogo from "~/assets/partners/offchain-labs.svg";
+import risepartnersLogo from "~/assets/partners/risepartners.png";
 
 const HomePartners: FC = () => {
   return (
@@ -17,7 +22,34 @@ const HomePartners: FC = () => {
         <h3 className="pb-16 text-center font-heading text-4xl font-bold text-fg/90">
           Our partners
         </h3>
-        <ul className="flex flex-wrap justify-center gap-16 px-16">
+        <ul className="flex flex-wrap justify-center items-center gap-16 px-16 [max-width:50vw]">
+          <li>
+            <Link
+              href="https://arbitrum.io/"
+              target="_blank"
+              className="inline-flex min-h-[60px] items-center justify-center rounded-2xl bg-gradient-radial from-bg/50 to-transparent opacity-80 backdrop-blur-md transition hover:opacity-100"
+            >
+              <Image src={arbitrumLogo} alt="Arbitrum Logo" height={45} />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://linea.build/"
+              target="_blank"
+              className="inline-flex min-h-[60px] items-center justify-center rounded-2xl bg-gradient-radial from-bg/50 to-transparent opacity-80 backdrop-blur-md transition hover:opacity-100"
+            >
+              <Image src={lineaLogo} alt="Linea Logo" height={40} />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://hedera.com/"
+              target="_blank"
+              className="inline-flex min-h-[60px] items-center justify-center rounded-2xl bg-gradient-radial from-bg/50 to-transparent opacity-80 backdrop-blur-md transition hover:opacity-100"
+            >
+              <Image src={hederaLogo} alt="Hedera Logo" height={45} />
+            </Link>
+          </li>
           <li>
             <Link
               href="https://www.circle.com"
@@ -29,6 +61,16 @@ const HomePartners: FC = () => {
           </li>
           <li>
             <Link
+              href="https://offchainlabs.com/"
+              target="_blank"
+              className="inline-flex min-h-[60px] items-center justify-center rounded-2xl bg-gradient-radial from-bg/50 to-transparent opacity-80 backdrop-blur-md transition hover:opacity-100"
+            >
+              <Image src={offchainLabsLogo} alt="Offchain Labs Logo" height={45} />
+            </Link>
+          </li>
+
+          <li>
+            <Link
               href="https://www.risepartners.org"
               target="_blank"
               className="inline-flex min-h-[60px] items-center justify-center rounded-2xl bg-gradient-radial from-bg/50 to-transparent opacity-80 backdrop-blur-md transition hover:opacity-100"
@@ -38,20 +80,11 @@ const HomePartners: FC = () => {
           </li>
           <li>
             <Link
-              href="https://www.adan.eu"
-              target="_blank"
-              className="inline-flex min-h-[60px] items-center justify-center rounded-2xl bg-gradient-radial from-bg/50 to-transparent opacity-80 backdrop-blur-md transition hover:opacity-100"
-            >
-              <Image src={adanLogo} alt="ADAN Logo" height={40} />
-            </Link>
-          </li>
-          <li>
-            <Link
               href="https://foundersdao.io/"
               target="_blank"
               className="inline-flex min-h-[60px] items-center justify-center rounded-2xl bg-gradient-radial from-bg/50 to-transparent opacity-80 backdrop-blur-md transition hover:opacity-100"
             >
-              <Image src={foundersoneLogo} alt="Founders One Logo" height={40} />
+              <Image src={foundersDAOLogo} alt="Founders DAO Logo" height={40} />
             </Link>
           </li>
           <li>
@@ -61,6 +94,24 @@ const HomePartners: FC = () => {
               className="inline-flex min-h-[60px] items-center justify-center rounded-2xl bg-gradient-radial from-bg/50 to-transparent opacity-80 backdrop-blur-md transition hover:opacity-100"
             >
               <Image src={delubacLogo} alt="Delubac Logo" height={40} />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://www.genius-x.co/"
+              target="_blank"
+              className="inline-flex min-h-[60px] items-center justify-center rounded-2xl bg-gradient-radial from-bg/50 to-transparent opacity-80 backdrop-blur-md transition hover:opacity-100"
+            >
+              <Image src={geniusXLogo} alt="GeniusX Logo" height={40} />
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="https://www.adan.eu"
+              target="_blank"
+              className="inline-flex min-h-[60px] items-center justify-center rounded-2xl bg-gradient-radial from-bg/50 to-transparent opacity-80 backdrop-blur-md transition hover:opacity-100"
+            >
+              <Image src={adanLogo} alt="ADAN Logo" height={35} />
             </Link>
           </li>
         </ul>
