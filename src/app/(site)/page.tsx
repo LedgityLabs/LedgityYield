ccf4843ee97c7b8279f507e9c6c9a720e4fab919
Lila Rest <mail@lila.rest>
2023-08-20 18:25:02 +0200

    perf: add opaque background to landing page's body to remove use of blur

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -13,7 +13,7 @@ const Page: NextPage = () => {
   return (
     <>
       <HomeHero className="fixed inset-0 h-[100vh] pt-[30px]" />
-      <div className="relative top-[100vh] z-10 border-t-2 border-t-primary/10 bg-[url('/assets/textures/other-glow.webp')] bg-cover bg-[left_30%_top_30%] pb-[100vh] pt-16 shadow-2xl shadow-primary/50 backdrop-blur-[10px] [border-radius:53%_47%_0%_0%/1%_1%_100%_100%] md:bg-top md:[border-radius:53%_47%_0%_0%/3%_3%_100%_100%]">
+      <div className="relative top-[100vh] z-10 border-t-2 border-t-primary/10 bg-[url('/assets/textures/other-glow.webp')] bg-[#D4DDFF] bg-cover bg-[left_30%_top_30%] pb-[100vh] pt-16 shadow-2xl shadow-primary/50 [border-radius:53%_47%_0%_0%/1%_1%_100%_100%] md:bg-top md:[border-radius:53%_47%_0%_0%/3%_3%_100%_100%]">
         <HomeFeatures />
         <HomeHowItWorks />
         <HomePartners />
