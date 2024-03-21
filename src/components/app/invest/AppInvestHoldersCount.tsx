f0bce07cf79ab031f933233994c4624d43c555f0
Lila Rest <mail@lila.rest>
2024-03-21 17:24:52 +0100

    feat: add dynamic holders count

--- src/components/app/invest/AppInvestHoldersCount.tsx
+++ src/components/app/invest/AppInvestHoldersCount.tsx
@@ -1,80 +1,70 @@
-import { Amount, Card, Spinner } from "@/components/ui";
+import { Spinner } from "@/components/ui";
+import { useAvailableLTokens } from "@/hooks/useAvailableLTokens";
 import { FC, useEffect, useState } from "react";
-// import { LToken, execute } from "graphclient";
-import { getTokenUSDRate } from "@/lib/getTokenUSDRate";
-import { parseUnits } from "viem";
-
-const availableChains = ["42161", "59144"];
+import { createPublicClient, erc20Abi, http, zeroAddress } from "viem";
+import { getContractEvents } from "viem/actions";
+import { arbitrum, linea } from "viem/chains";
+import { getContractAddress } from "@/lib/getContractAddress";
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {}
 
+const availableChains = [
+  {
+    id: "42161",
+    startBlock: 211050n,
+    viemObj: arbitrum,
+  },
+  {
+    id: "59144",
+    startBlock: 211050n,
+    viemObj: linea,
+  },
+];
+
 export const AppInvestHoldersCount: FC<Props> = (props) => {
-  // const [totalMintedRewardsUsd, setTotalMintedRewardsUsd] = useState<bigint | "N/A">(0n);
-  // const [isLoading, setIsLoading] = useState(false);
+  const [holdersCount, setHoldersCount] = useState<number | "N/A">("N/A");
+  const [isLoading, setIsLoading] = useState(false);
+  const lTokens = useAvailableLTokens();
 
-  // const computeTotalMintedRewardsUsd = async () => {
-  //   setIsLoading(true);
+  const computeHoldersCount = async () => {
+    setIsLoading(true);
+    let totalHolders = 0;
+    for (const chain of availableChains) {
+      const client = createPublicClient({
+        chain: chain.viemObj,
+        transport: http(),
+      });
+      for (const lTokenSymbol of lTokens) {
+        const lTokenAddress = getContractAddress(lTokenSymbol, chain.id);
+        // Retrieve all L-Token transfers events
+        const logs = await getContractEvents(client, {
+          address: lTokenAddress,
+          abi: erc20Abi,
+          eventName: "Transfer",
+          fromBlock: chain.startBlock,
+          toBlock: "latest",
+        });
 
-  //   // Build GraphQL query string
-  //   let queryString = "{\n";
-  //   for (const chainId of availableChains) {
-  //     queryString += `
-  //       c${chainId}_ltokens {
-  //         totalMintedRewards
-  //         symbol
-  //         decimals
-  //       }`;
-  //   }
-  //   queryString += "\n}";
+        // Retrieve all current and past holders
+        const holders = new Set<string>();
+        logs.forEach((log) => {
+          if (log.args.from && ![lTokenAddress, zeroAddress].includes(log.args.from))
+            holders.add(log.args.from);
+          if (log.args.to && ![lTokenAddress, zeroAddress].includes(log.args.to))
+            holders.add(log.args.to);
+        });
 
-  //   return execute(queryString, {})
-  //     .then(
-  //       async (result: {
-  //         data: {
-  //           [key: string]: LToken[];
-  //         };
-  //       }) => {
-  //         let newTotalMintedRewardsUsd = 0n;
-  //         const proms: Promise<void>[] = [];
+        // Update total holders count
+        totalHolders += holders.size;
+      }
+    }
+    setHoldersCount(totalHolders);
+    setIsLoading(false);
+  };
 
-  //         for (const chainId of availableChains) {
-  //           const rewardsMintsData = result.data[`c${chainId}_ltokens`];
-  //           for (const lToken of rewardsMintsData) {
-  //             proms.push(
-  //               getTokenUSDRate(lToken.symbol.slice(1)).then((usdRate) => {
-  //                 newTotalMintedRewardsUsd +=
-  //                   (BigInt(lToken.totalMintedRewards) *
-  //                     parseUnits(usdRate.toString(), lToken.decimals)) /
-  //                   parseUnits("1", lToken.decimals);
-  //               }),
-  //             );
-  //           }
-  //         }
+  useEffect(() => {
+    computeHoldersCount();
+  }, []);
 
-  //         await Promise.all(proms).then(() => {
-  //           setTotalMintedRewardsUsd(newTotalMintedRewardsUsd);
-  //           setIsLoading(false);
-  //         });
-  //       },
-  //     )
-  //     .catch((e: Error) => {
-  //       setTotalMintedRewardsUsd("N/A");
-  //       setIsLoading(false);
-  //     });
-  // };
-  // useEffect(() => {
-  //   computeTotalMintedRewardsUsd();
-  // }, []);
-
-  return (
-    <div {...props}>
-      {/* {(isLoading && <Spinner />) ||
-        (totalMintedRewardsUsd !== "N/A" ? (
-          <Amount prefix="$" value={totalMintedRewardsUsd} decimals={6} />
-        ) : (
-          "N/A"
-        ))} */}
-      653
-    </div>
-  );
+  return <div {...props}>{(isLoading && <Spinner />) || holdersCount}</div>;
 };
