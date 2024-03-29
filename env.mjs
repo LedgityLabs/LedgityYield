2c35d78a4618d720cc361b42edc0cc05f5a8a173
Lila Rest <mail@lila.rest>
2024-03-29 08:45:24 +0100

    fix: remove unused env variables, and mark as optional the airdrop ones

--- env.mjs
+++ env.mjs
@@ -3,21 +3,16 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    SCORECHAIN_API_URL: z.string().url(),
-    SCORECHAIN_API_KEY: z.string(),
-    ZEALY_API_KEY: z.string(),
-    TWITTER_CLIENT_ID: z.string(),
-    TWITTER_CLIENT_SECRET: z.string(),
-    REDIS_HOST: z.string(),
-    REDIS_USERNAME: z.string(),
-    REDIS_PASSWORD: z.string(),
+    TWITTER_CLIENT_ID: z.string().optional(),
+    TWITTER_CLIENT_SECRET: z.string().optional(),
+    REDIS_HOST: z.string().optional(),
+    REDIS_USERNAME: z.string().optional(),
+    REDIS_PASSWORD: z.string().optional(),
   },
   client: {
-    NEXT_PUBLIC_THEGRAPH_API_KEY: z.string().min(1),
     NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: z.string(),
   },
   experimental__runtimeEnv: {
-    NEXT_PUBLIC_THEGRAPH_API_KEY: process.env.NEXT_PUBLIC_THEGRAPH_API_KEY,
     NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
   },
 });
