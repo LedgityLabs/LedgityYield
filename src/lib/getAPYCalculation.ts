f76d51239f828018e02f87192f45dbd97876a4c2
painiteleo <painiteleo@gmail.com>
2024-05-26 23:45:23 +0800

    fix: aPY calculation

--- src/lib/getAPYCalculation.ts
+++ src/lib/getAPYCalculation.ts
@@ -2,23 +2,21 @@ import { OneMonth, StakeDurationMultipliers, MULTIPLIER_BASIS } from "@/constant
 import { parseUnits } from "viem";
 
 export const getAPYCalculation = (
-  rewardRate: number,
+  rewardRatePerSec: number,
   totalWeightedStake: number,
   stakeDurationIndex: number,
 ) => {
-  // -------- APR and APY Formula ----------- //
-
   const OneToken = Number(parseUnits("1", 18));
   const OneYear = 12 * OneMonth;
-  if (totalWeightedStake == 0) {
-    totalWeightedStake = OneToken;
-  }
 
-  const rewardRatePerSecPerToken = (rewardRate * OneToken) / totalWeightedStake;
-  const rewardRatePerYearPerToken = rewardRatePerSecPerToken * OneYear;
+  const rewardRatePerYear = rewardRatePerSec * OneYear;
   const multiplier = StakeDurationMultipliers[stakeDurationIndex];
   const myStakeAmount = (OneToken * multiplier) / MULTIPLIER_BASIS;
+  if (totalWeightedStake == 0) {
+    totalWeightedStake = myStakeAmount;
+  }
   const myPortion = myStakeAmount / totalWeightedStake;
-  const APY = rewardRatePerYearPerToken * myPortion * 100;
+  const rewardsAmountPerYear = rewardRatePerYear * myPortion;
+  const APY = (rewardsAmountPerYear / OneToken) * 100;
   return APY.toFixed(2);
 };
