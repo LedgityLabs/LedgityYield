00e04e1fda8c9bd53501c39520159e7e0aa050f6
painiteleo <painiteleo@gmail.com>
2024-05-29 15:21:31 +0800

    fix: multipler contant values in the frontend

--- src/constants/staking.ts
+++ src/constants/staking.ts
@@ -1,5 +1,5 @@
 export const StakeDurations = [0, 1, 6, 12];
-export const StakeDurationMultipliers = [10000, 40000, 15000, 20000];
+export const StakeDurationMultipliers = [10000, 40000, 60000, 100000];
 export const MULTIPLIER_BASIS = 10000;
 export const OneMonth = 31 * 24 * 60 * 60;
 export const STAKING_APR_INFO_ID = "STAKING_APR_INFO_ID";
