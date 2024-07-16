0d12a825fb7874e425a901ab129d3f564b3bde92
torof <scottpanam@protonmail.com>
2024-07-16 23:58:50 +0900

    style(width styling): make the component wider

--- src/components/app/bridge/AppBridge.tsx
+++ src/components/app/bridge/AppBridge.tsx
@@ -1,6 +1,9 @@
 import '@swing.xyz/ui/theme.css';
 import { Swap } from '@swing.xyz/ui';
+import "./AppBridge.css";
 
 export function AppBridge() {
-  return <Swap projectId="swing-bridge" environment='production'/>;
+  return <div id="swing">
+      <Swap projectId="swing-bridge" environment='production' />
+    </div>
 }
