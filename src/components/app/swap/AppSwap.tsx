112ac1b3dd8fdbef2d8dc7f89487809cd1906e01
torof <scottpanam@protonmail.com>
2024-07-20 10:45:42 +0900

    style(responsive widget): changed of styles of swing widget to be responsive, removed tooltip
    fix #139

--- src/components/app/swap/AppSwap.tsx
+++ src/components/app/swap/AppSwap.tsx
@@ -1,8 +1,11 @@
 import '@swing.xyz/ui/theme.css';
+import "./AppSwap.css";
 import { Swap } from '@swing.xyz/ui';
 
 export function AppSwap() {
-  return <div className="w-[800px]">
+  return (
+    <div className="app-swap-container">
       <Swap projectId="swing-bridge" environment='production' />
     </div>
+  );
 }
