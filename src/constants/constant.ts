44766eb8629277a58d3984ce1f5021e010323d72
painiteleo <painiteleo@painiteleos-iMac-Pro.local>
2024-07-22 06:34:22 +0800

    perf: change frontend url

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,2 +1,2 @@
 export const AFFILIATE_API_URL = "https://ledgity-affiliate-backend.onrender.com";
-export const FRONTEND_URL = "http://localhost:3000";
+export const FRONTEND_URL = "https://ledgity-yield.vercel.app";
