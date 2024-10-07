2335a3ffe2386bcc2f7e9fa7f0617e754a178326
Torof <dev.solidity@proton.me>
2024-10-07 09:09:11 +0200

    Delete frontend/.next directory

--- frontend/.next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/home/torof/Desktop/LedgityYield/frontend/src/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\",\"static/media/0484562807a97172-s.p.woff2\",\"static/media/b957ea75a84b6ea7-s.p.woff2\",\"static/media/7db6c35d839a711c-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
