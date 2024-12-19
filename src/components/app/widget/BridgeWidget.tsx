7a97e94d222c9a5a56a0a20a82e3ed66a8ecc4aa
blackwhale.eth <victor.blackwhale@gmail.com>
2024-12-19 19:29:20 +0100

    chore (ui): add padding to widgets

--- src/components/app/widget/BridgeWidget.tsx
+++ src/components/app/widget/BridgeWidget.tsx
@@ -5,14 +5,14 @@ export const dstToken = "0x999FAF0AF2fF109938eeFE6A7BF91CA56f0D07e1";
 
 export function BridgeWidget() {
   return (
-    <TxWidget
-      integratorId={process.env.NEXT_PUBLIC_INTEGRATOR_ID || ""}
-      dstChain={dstChain}
-      dstToken={dstToken}
-      lightTheme={true}
-      defaultWalletPicker={true}
-    />
+    <div className="pb-8">
+      <TxWidget
+        integratorId={process.env.NEXT_PUBLIC_INTEGRATOR_ID || ""}
+        dstChain={dstChain}
+        dstToken={dstToken}
+        lightTheme={true}
+        defaultWalletPicker={true}
+      />
+    </div>
   );
 }
-
-
--- src/components/app/widget/SwapWidget.tsx
+++ src/components/app/widget/SwapWidget.tsx
@@ -3,7 +3,7 @@ import { Swap } from "@swing.xyz/ui";
 
 export function SwapWidget() {
   return (
-    <div className="w-[90vw] max-w-[800px] mx-auto sm:w-[95vw] xs:w-screen xs:px-2.5">
+    <div className="w-[90vw] max-w-[800px] mx-auto sm:w-[95vw] xs:w-screen xs:px-2.5 pb-8">
       <Swap projectId="swing-bridge" environment="production" />
     </div>
   );
