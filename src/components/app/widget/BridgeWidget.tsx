3980541576a835d013a5f52c0c90be3ce7486af8
blackwhale.eth <victor.blackwhale@gmail.com>
2024-12-20 16:42:57 +0100

    chore (bridge): bridge ldy + minor npm up

--- src/components/app/widget/BridgeWidget.tsx
+++ src/components/app/widget/BridgeWidget.tsx
@@ -1,13 +1,17 @@
 import { TxWidget } from "@xswap-link/sdk";
 
-export const dstChain = "42161";
-export const dstToken = "0x999FAF0AF2fF109938eeFE6A7BF91CA56f0D07e1";
+const srcChain = "42161";
+const dstChain = "8453";
+const srcToken = "0x999FAF0AF2fF109938eeFE6A7BF91CA56f0D07e1";
+const dstToken = "0x055d20a70eFd45aB839Ae1A39603D0cFDBDd8a13";
 
 export function BridgeWidget() {
   return (
     <div className="pb-8">
       <TxWidget
         integratorId={process.env.NEXT_PUBLIC_INTEGRATOR_ID || ""}
+        srcChain={srcChain}
+        srcToken={srcToken}
         dstChain={dstChain}
         dstToken={dstToken}
         lightTheme={true}
