a2ace26d73a83ee238856b336ce4328723792aa8
blackwhale.eth <victor.blackwhale@gmail.com>
2025-03-05 15:17:29 +0100

    fix (hooks): bad conditions for hooks & inverted params

--- src/hooks/contracts/read/useCanInstantWithdraw.ts
+++ src/hooks/contracts/read/useCanInstantWithdraw.ts
@@ -10,15 +10,16 @@ import { Address, zeroAddress } from "viem";
 /**
  * Checks the core conditions for an instant withdrawal to succeed
  * Simplified from the LToken contract
+ *
+ * returns FALSE if cannot instant withdraw
+ * returns UNDEFINED if data is not loaded yet
  */
 export function useCanInstantWithdraw(
-  account: Address | undefined,
   lTokenAddress: Address | undefined,
+  account: Address | undefined,
   amount: bigint,
 ): boolean | undefined {
   const { appChainId } = useWeb3Context();
-
-  if (!lTokenAddress) return undefined;
 
   const { data: usableUnderlyings } = useReadLTokenUsableUnderlyings({
     // @dev Chain ID typesafety doing its job but getting in the way here
@@ -56,17 +57,19 @@ export function useCanInstantWithdraw(
     },
   });
 
-  // If only high tier can use instant withdrawals and user isn't high tier
-  if (onlyHighTier && isHighTier === false) {
-    return false;
-  }
-
+  // If any data is missing, return undefined
   if (
+    !lTokenAddress ||
     usableUnderlyings === undefined ||
     totalQueued === undefined ||
     isHighTier === undefined
   )
     return undefined;
+
+  // If only high tier can use instant withdrawals and user isn't high tier
+  if (onlyHighTier && isHighTier === false) {
+    return false;
+  }
 
   // Check liquidity conditions
   const hasLiquidityForQueue = totalQueued + amount <= usableUnderlyings;
