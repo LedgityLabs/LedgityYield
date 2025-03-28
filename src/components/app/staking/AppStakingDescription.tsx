87e40a466f210787c88029888dce0fc24c9d90db
blackwhale.eth <victor.blackwhale@gmail.com>
2025-03-28 14:33:00 +0100

    chore (comp): change staking program content

--- src/components/app/staking/AppStakingDescription.tsx
+++ src/components/app/staking/AppStakingDescription.tsx
@@ -5,11 +5,7 @@ export const AppStakingDescription: FC = () => {
     <div className="flex flex-col justify-start gap-y-2 p-4">
       <div className="font-heading font-bold text-xl">YOUR PERKS</div>
       <div className="font-heading font-semibold text-lg py-2">
-        Stake at least 1000 $LDY for 12months
-      </div>
-      <div className="font-medium flex items-center">
-        <i className="ri-checkbox-circle-line text-xl" />
-        <span className="mx-1">Protocol profits sharing</span>
+        Stake at least 10,000 $LDY for 12months
       </div>
       <div className="font-medium flex items-center">
         <i className="ri-checkbox-circle-line text-xl" />
@@ -25,11 +21,7 @@ export const AppStakingDescription: FC = () => {
       </div>
       <div className="font-medium flex items-center">
         <i className="ri-checkbox-circle-line text-xl" />
-        <span className="mx-1">Voting power</span>
-      </div>
-      <div className="font-medium flex items-center">
-        <i className="ri-checkbox-circle-line text-xl" />
-        <span className="mx-1">Visibility on the portfolio of RWA</span>
+        <span className="mx-1">Advanced overview of the RWA Portfolio</span>
       </div>
     </div>
   );
